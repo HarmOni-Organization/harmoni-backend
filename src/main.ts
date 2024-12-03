aa3342d53fa4195f5174930d802d577233d943dc
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2024-12-03 10:23:18 +0200

    add the request logger Interceptors globally

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
+  // Enable request logger Interceptor globally
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   // Enable validation globally
   app.useGlobalPipes(
