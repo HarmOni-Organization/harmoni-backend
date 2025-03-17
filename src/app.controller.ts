5849b484791d6f7bf14edb11c1293cf2f3a9eb64
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2025-03-17 12:33:31 +0200

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -68,20 +68,6 @@ export class AppController {
     };
   }
 
-  @Patch('items/:id')
-  partialUpdateItem(@Param('id') id: string, @Body() patchItemDto: any) {
-    return {
-      message: `This endpoint partially updates an item with ID: ${id}`,
-      method: 'PATCH',
-      receivedData: patchItemDto,
-      patchedItem: {
-        id: parseInt(id),
-        name: `Item ${id} (patched)`,
-        ...patchItemDto,
-      },
-    };
-  }
-
   @Delete('items/:id')
   deleteItem(@Param('id') id: string) {
     return {
